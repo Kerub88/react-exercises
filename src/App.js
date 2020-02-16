50a42e95749a9ba5496096ed047c0ac13803a93b
unknown <kacsa1984@gmail.com>
2020-02-16 16:30:32 +0100

    feat(card-list) component created

--- src/App.js
+++ src/App.js
@@ -1,42 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { CardList } from './components/card-list/card-list.component'
+
 import './App.css';
 
 class App extends Component {
-constructor() {
-  super();
+  constructor() {
+    super();
 
-  this.state = {
-    monsters: [
-      {
-        name: 'Frankenstein'
-      },
-      {
-        name: 'Dracula'
-      },
-      {
-        name: 'Zombie'
-      }
-    ]
-  };
-}
+    this.state = {
+      monsters: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users}))
+  }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button onClick={() => this.setState({ string: 'Téged is szeret Jézus'})}>Change text</button>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <CardList monsters={this.state.monsters}>
+          {this.state.monsters.map(monster => (
+            <h1 key={monster.id}>{monster.name}</h1>
+            ))}
+        </CardList>
       </div>
     )
   }
